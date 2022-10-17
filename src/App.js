4f29a29bbe544d6d9874b31212814259530bbcc0
dlswns6532 <hij092919@gmail.com>
2022-10-17 16:19:08 +0900

    TodoItem UI test ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 
 function App() {
   return (
     <div className="App">
       <TodoForm />
+      <TodoItem />
     </div>
   );
 }
