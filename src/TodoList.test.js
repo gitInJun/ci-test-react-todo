eb29b85af33d33200ae5193c935071ba85649daa
dlswns6532 <hij092919@gmail.com>
2022-10-18 10:10:54 +0900

    TodoList test ok

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, getByPlaceholderText, render } from '@testing-library/react';
+import { render, fireEvent, getByText, getAllByAltText } from '@testing-library/react';
 import TodoList from './TodoList';
 
 describe('<TodoList />', () => {
@@ -11,7 +11,7 @@ describe('<TodoList />', () => {
         },
         {
             id: 2,
-            text: 'reacting-testing-library',
+            text: 'react-testing-library',
             done: true,
         }
     ];
@@ -19,5 +19,16 @@ describe('<TodoList />', () => {
         const { getByText } = render(<TodoList todos={sampleTodos} />);
         getByText(sampleTodos[0].text);
         getByText(sampleTodos[1].text);
+    });
+
+    it('calls onToggle and onRemove', () => {
+        const onToggle = jest.fn();
+        const onRemove = jest.fn();
+        const { getByText, getAllByText } = render(<TodoList todos={sampleTodos}
+            onToggle={onToggle} onRemove={onRemove} />);
+        fireEvent.click(getByText(sampleTodos[0].text));
+        expect(onToggle).toBeCalledWith(sampleTodos[0].id);
+        fireEvent.click(getAllByText('삭제')[0]);
+        expect(onRemove).toBeCalledWith(sampleTodos[0].id);
     });
 });
